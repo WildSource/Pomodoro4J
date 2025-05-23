617654dfa2e8aa0edfd43b704540fc49e625b8ea
WildSource <ilovetrap123@hotmail.com>
2025-05-22 22:09:19 -0400

    Add unfinished Pomodoro Controller

---
+++ src/main/java/org/example/controllers/PomodoroController.java
@@ -0,0 +1,19 @@
+package org.example.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class PomodoroController {
+    @FXML
+    private Label title;
+    @FXML
+    private Label timer;
+    @FXML
+    private Label previous;
+    @FXML
+    private Label next;
+    @FXML
+    private Button completeTask;
+
+}
